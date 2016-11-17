6ae4e3247fb76e8308a8c5b48d646b3f23520968
Song Tian <stian3@cs.ualberta.ca>
2016-11-17 12:42:56 -0700

    LHS RHS

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ c = conn.cursor()
 
 def connectDatabase():
 
-    # gives us table names
+    # GET TABLE NAMES
     c.execute("SELECT name FROM sqlite_master;")
     table_names = c.fetchall()
     print table_names
-    
     # 1 list of the table names
     table = []
     for i in range(len(table_names)):
@@ -17,7 +16,7 @@ def connectDatabase():
             table.append(str(table_names[i][j]))
     print table 
     
-    # getting the column names
+    # GET COLUMN NAMES
     c.execute("PRAGMA table_info(Input_R1)")
     col_names = c.fetchall()
     print col_names
@@ -26,9 +25,27 @@ def connectDatabase():
     for i in range (len(col_names)):
         col.append(str(col_names[i][1]))
     print col
+    
+    # GET FUNCTIONAL DEPENDENCIES
+    # get LHS
+    c.execute("select LHS from Input_FDs_R1;")
+    lhs = c.fetchall()
+    print lsh
+    LHS = []
+    for i in range(len(lhs)):
+        for j in range(len(lhs[i])):
+            LHS.append(str(lhs[i][j]))
+    print LHS
+    
+    
+    
             
         
-   
+############################
+#         MAIN             #
+############################   
 connectDatabase()
+
+
     
 
